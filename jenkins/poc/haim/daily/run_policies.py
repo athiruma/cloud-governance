fe90392c319f666899f251cddd5630e43d3f6c5c
AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>
2023-06-18 22:24:59 +0530

    Added the haim reports

--- jenkins/poc/haim/daily/run_policies.py
+++ jenkins/poc/haim/daily/run_policies.py
@@ -1,14 +1,16 @@
-
+import json
 import os
 
 
-AWS_ACCESS_KEY_ID_APPENG = os.environ['AWS_ACCESS_KEY_ID_APPENG']
+HAIM_CREDS = os.environ['HAIM_CREDS']
 AWS_SECRET_ACCESS_KEY_APPENG = os.environ['AWS_SECRET_ACCESS_KEY_APPENG']
 LDAP_HOST_NAME = os.environ['LDAP_HOST_NAME']
 LOGS = os.environ.get('LOGS', 'logs')
 ES_HOST = os.environ['ES_HOST']
 ES_PORT = os.environ['ES_PORT']
 BUCKET_APPENG = os.environ['BUCKET_APPENG']
+GOOGLE_APPLICATION_CREDENTIALS = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+SPREADSHEET_ID = os.environ['AWS_IAM_USER_SPREADSHEET_ID']
 
 
 def get_policies(type: str = None):
@@ -34,20 +36,48 @@ not_action_policies = ['cost_explorer', 'cost_over_usage', 'monthly_report', 'co
 run_policies = list(set(policies) - set(not_action_policies))
 run_policies.sort()
 
+with open(HAIM_CREDS) as file:
+    aws_accounts = json.load(file)
 
-os.system(f"""echo Running the cloud_governance policies: {run_policies}""")
-os.system(f"""echo "Running the CloudGovernance policies" """)
-for region in regions:
-    for policy in run_policies:
-        if policy in ('empty_roles', 's3_inactive') and region == 'us-east-1':
-            os.system(f"""podman run --rm --name cloud-governance-poc-haim --net="host" -e MANAGER_EMAIL_ALERT="False" -e EMAIL_ALERT="False" -e account="APPENG" -e policy="{policy}" -e AWS_ACCESS_KEY_ID="{AWS_ACCESS_KEY_ID_APPENG}" -e AWS_SECRET_ACCESS_KEY="{AWS_SECRET_ACCESS_KEY_APPENG}" -e AWS_DEFAULT_REGION="{region}" -e dry_run="yes" -e LDAP_HOST_NAME="{LDAP_HOST_NAME}" -e es_host="{ES_HOST}" -e es_port="{ES_PORT}" -e policy_output="s3://{BUCKET_APPENG}/{LOGS}/{region}" -e log_level="INFO" quay.io/ebattat/cloud-governance:latest""")
-        else:
-            os.system(f"""podman run --rm --name cloud-governance-poc-haim --net="host" -e MANAGER_EMAIL_ALERT="False" -e EMAIL_ALERT="False" -e account="APPENG" -e policy="{policy}" -e AWS_ACCESS_KEY_ID="{AWS_ACCESS_KEY_ID_APPENG}" -e AWS_SECRET_ACCESS_KEY="{AWS_SECRET_ACCESS_KEY_APPENG}" -e AWS_DEFAULT_REGION="{region}" -e dry_run="yes" -e LDAP_HOST_NAME="{LDAP_HOST_NAME}" -e es_host="{ES_HOST}" -e es_port="{ES_PORT}" -e policy_output="s3://{BUCKET_APPENG}/{LOGS}/{region}" -e log_level="INFO" quay.io/ebattat/cloud-governance:latest""")
-
-
-cost_tags = ['PurchaseType', 'ChargeType', 'User', 'Budget', 'Project', 'Manager', 'Owner', 'LaunchTime', 'Name', 'Email', 'Environment', 'User:Spot']
+cost_tags = ['PurchaseType', 'ChargeType', 'User', 'Budget', 'Project', 'Manager', 'Owner', 'LaunchTime', 'Environment', 'User:Spot']
 cost_metric = 'UnblendedCost'  # UnblendedCost/BlendedCost
 granularity = 'DAILY'  # DAILY/MONTHLY/HOURLY
 cost_explorer_index = 'cloud-governance-haim-cost-explorer-global-index'
 os.system(f"""echo "Running the CloudGovernance CostExplorer Policies" """)
-os.system(f"""podman run --rm --name cloud-governance -e AWS_DEFAULT_REGION="us-east-1" -e account="appeng" -e policy="cost_explorer" -e AWS_ACCESS_KEY_ID="{AWS_ACCESS_KEY_ID_APPENG}" -e AWS_SECRET_ACCESS_KEY="{AWS_SECRET_ACCESS_KEY_APPENG}" -e es_host="{ES_HOST}" -e es_port="{ES_PORT}" -e es_index="{cost_explorer_index}" -e cost_explorer_tags="{cost_tags}" -e granularity="{granularity}" -e cost_metric="{cost_metric}" -e log_level="INFO" quay.io/ebattat/cloud-governance:latest""")
+for account_name, account_values in aws_accounts.items():
+    access_id = account_values.get("AWS_ACCESS_KEY_ID")
+    secret_key = account_values.get('AWS_SECRET_ACCESS_KEY')
+    bucket = account_values.get('BUCKET')
+    os.system(f"""podman run --rm --name cloud-governance-daily --net="host" -e account="{account_name}" -e AWS_ACCESS_KEY_ID="{access_id}" -e AWS_SECRET_ACCESS_KEY="{secret_key}"  -e AWS_DEFAULT_REGION="us-east-1"  -e policy="cost_explorer"  -e es_host="{ES_HOST}" -e es_port="{ES_PORT}" -e es_index="{cost_explorer_index}" -it -e cost_explorer_tags="{cost_tags}" -e granularity="{granularity}" -e cost_metric="{cost_metric}" -e policy_output="s3://{bucket}/{LOGS}/us-east-1" -e log_level="INFO" quay.io/ebattat/cloud-governance:latest""")
+
+# run_policies = ['ebs_unattached', 'ip_unattached', 's3_inactive']
+
+os.system(f"""echo Running the cloud_governance policies: {run_policies}""")
+os.system(f"""echo "Running the CloudGovernance policies" """)
+for account_name, account_values in aws_accounts.items():
+    access_id = account_values.get("AWS_ACCESS_KEY_ID")
+    secret_key = account_values.get('AWS_SECRET_ACCESS_KEY')
+    bucket = account_values.get('BUCKET')
+    for region in regions:
+        for policy in run_policies:
+            if policy in ('empty_roles', 's3_inactive') and region == 'us-east-1':
+                os.system(f"""podman run --rm --name cloud-governance-daily --net="host" -e account="{account_name}" -e AWS_ACCESS_KEY_ID="{access_id}" -e AWS_SECRET_ACCESS_KEY="{secret_key}" -e MANAGER_EMAIL_ALERT="False" -e EMAIL_ALERT="False"  -e policy="{policy}" -e AWS_DEFAULT_REGION="{region}" -e dry_run="yes" -e LDAP_HOST_NAME="{LDAP_HOST_NAME}" -e es_host="{ES_HOST}" -e es_port="{ES_PORT}" -e log_level="INFO" -e policy_output="s3://{bucket}/{LOGS}/{region}" quay.io/ebattat/cloud-governance:latest""")
+            elif policy not in ('empty_roles', 's3_inactive'):
+                os.system(f"""podman run --rm --name cloud-governance-daily --net="host" -e account="{account_name}" -e AWS_ACCESS_KEY_ID="{access_id}" -e AWS_SECRET_ACCESS_KEY="{secret_key}" -e MANAGER_EMAIL_ALERT="False" -e EMAIL_ALERT="False" -e policy="{policy}" -e AWS_DEFAULT_REGION="{region}" -e dry_run="yes" -e LDAP_HOST_NAME="{LDAP_HOST_NAME}" -e es_host="{ES_HOST}" -e es_port="{ES_PORT}" -e log_level="INFO" -e policy_output="s3://{bucket}/{LOGS}/{region}" quay.io/ebattat/cloud-governance:latest""")
+
+
+os.system(f"""echo "Running the tag_iam_user" """)
+for account_name, account_values in aws_accounts.items():
+    access_id = account_values.get("AWS_ACCESS_KEY_ID")
+    secret_key = account_values.get('AWS_SECRET_ACCESS_KEY')
+    bucket = account_values.get('BUCKET')
+    os.system(f"""podman run --rm --name cloud-governance-hourly --net="host" \
+    -e account="{account_name}" \
+    -e AWS_ACCESS_KEY_ID="{access_id}" -e AWS_SECRET_ACCESS_KEY="{secret_key}" \
+    -e EMAIL_ALERT="False" \
+    -e policy="tag_iam_user" \
+    -e user_tag_operation="update"  \
+    -e SPREADSHEET_ID="{SPREADSHEET_ID}" \
+    -e GOOGLE_APPLICATION_CREDENTIALS="{GOOGLE_APPLICATION_CREDENTIALS}" \
+    -v "{GOOGLE_APPLICATION_CREDENTIALS}":"{GOOGLE_APPLICATION_CREDENTIALS}" \
+    -e LDAP_HOST_NAME="{LDAP_HOST_NAME}" -e log_level="INFO" quay.io/ebattat/cloud-governance:latest""")
